bb777d86c8420dcf6e749518c2bee42f18f52337
rafael399 <rafael399@gmail.com>
2021-12-04 14:52:53 -0300

    Add test to check if price is being converted right

--- tests/Clients/BestBuyTest.php
+++ tests/Clients/BestBuyTest.php
@@ -7,6 +7,7 @@ use App\Models\Stock;
 use Database\Seeders\RetailerWithProductSeeder;
 use Exception;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 /**
@@ -27,11 +28,21 @@ class BestBuyTest extends TestCase
         ]);
 
         try {
-            $stockStatus = (new BestBuy())->checkAvailability($stock);
+            (new BestBuy())->checkAvailability($stock);
         } catch (Exception $e) {
-            $this->fail('Failed to track the BestBuy API properly.');
+            $this->fail('Failed to track the BestBuy API properly. ' . $e->getMessage());
         }
 
         $this->assertTrue(true);
+    }
+
+    /** @test */
+    function it_creates_the_proper_stock_status_response()
+    {
+        Http::fake(fn () => ['salePrice' => 299.99, 'onlineAvailability' => true]);
+        $stockStatus = (new BestBuy())->checkAvailability(new Stock);
+
+        $this->assertEquals(29999, $stockStatus->price);
+        $this->assertEquals(true, $stockStatus->available);
     }
 }
